d41af1c03cc2ba39ee31f5c60f324511e35c0669
Cesar <c.augusto_mendez@hotmail.com>
2023-05-15 21:28:10 -0400

    Se corrige la funcionalidad del boton de eliminar y mostrar reservas

--- src/presenter.js
+++ src/presenter.js
@@ -13,29 +13,44 @@ var item=new Item("Pique Macho",15,20,"Carne y chorizo con papas.");
 
 //cliente.agregarReserva(item);
 
+productos.forEach((producto) => {
+  const li = document.createElement("li");
+  li.innerHTML = `${producto.nombre}: ${producto.descripcion} - Precio: $${producto.precio} - Stock: ${producto.stock}`;
+  productos_lista.appendChild(li);
 
-productos.forEach(producto => {
-        const li = document.createElement("li");
-        li.innerHTML = `${producto.nombre}: ${producto.descripcion} - Precio: $${producto.precio} - Stock: ${producto.stock}` ;
-        const agregarButton = document.createElement('Button');
-        agregarButton.textContent= "Agregar";
-        agregarButton.onclick = ()=>cliente.agregarReserva(producto);
-        productos_lista.appendChild(li);
-        productos_lista.append(agregarButton);
-          });
-   
-     
-var reservasCliente = cliente.reservas;
+  const agregarButton = document.createElement('button');
+  agregarButton.textContent = "Agregar";
+  agregarButton.addEventListener('click', () => {
+    cliente.agregarReserva(producto);
+  });
+
+  productos_lista.appendChild(agregarButton);
+});
+
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    reservasCliente.forEach((item) => {
-        const EliminarButton = document.createElement('Button');
-        EliminarButton.textContent= "Eliminar";
-        EliminarButton.onclick = ()=>cliente.eliminarReserva(producto);
-        const li = document.createElement("li");
-        li.innerHTML = `${item.nombre}: ${item.descripcion} - Precio: $${item.precio} - Stock: ${item.stock}`;
-        div.appendChild(li);
-      });
-      div.reset();
+  event.preventDefault();
+  const reservasCliente = cliente.reservas;
+
+  // Vaciar el contenido del div antes de agregar los elementos actualizados
+  div.innerHTML = "";
+
+  reservasCliente.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${item.nombre}: ${item.descripcion} - Precio: $${item.precio} - Stock: ${item.stock}`;
+    div.appendChild(li);
+
+    const eliminarButton = document.createElement('button');
+    eliminarButton.textContent = "Eliminar";
+    eliminarButton.addEventListener('click', () => {
+      cliente.eliminarReserva(item);
+      div.removeChild(li);
+      div.removeChild(eliminarButton);
+    });
+
+    div.appendChild(eliminarButton);
+  });
 });
+
+
+
