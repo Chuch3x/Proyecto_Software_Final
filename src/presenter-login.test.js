36a29cfa03bc54e9468c8354ce6e5dd9e979953c
Dayan <dayan.aguilar@ucb.edu.bo>
2023-05-23 20:58:07 -0400

    agregamos el test que verifica la funcion de validar datos

--- src/presenter-login.test.js
+++ src/presenter-login.test.js
@@ -1,8 +1,11 @@
 import Cliente from "./cliente";
 
 describe("Mostrar items", () => {
-    var admin = new Cliente("password","admin");
-    it("deberia verificar si es administrado", () => {
-     expect(admin.esAdmin()).toEqual(true);
-     });
-})
+  var admin = new Cliente("password", "admin");
+  it("deberia verificar si es administrado", () => {
+    expect(admin.esAdmin()).toEqual(true);
+  });
+  it("deberia validar los datos", () => {
+    expect(new Cliente("", "").validarDatos()).toEqual(false);
+  });
+});
