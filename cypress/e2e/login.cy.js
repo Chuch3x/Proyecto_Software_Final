5a98975139c06bba48d3fefcd6fa73f7a83864e5
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-05-31 16:31:13 -0400

    Cypress: No debería iniciar sesión

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,18 @@
-describe('Iniciar Sesión', () => {
+describe('Inicio de Sesión', () => {
     it('Debería iniciar sesión como admin exitosamente', () => {
         cy.visit('login.html')
         cy.get('#usuario').type('admin')
         cy.get('#contrasenia').type('password')    
         cy.get('button[type="submit"]').click()
+        cy.get("#logout_admin").should('be.visible');
     });
+    it('No debería iniciar sesión exitosamente', () => {
+        cy.visit('login.html')
+        cy.get('#usuario').type('sads')
+        cy.get('#contrasenia').type('pafdfdssword')    
+        cy.get('button[type="submit"]').click()
+        cy.get("#logout_admin").should('not.exist');
+    });
+
+
 });
