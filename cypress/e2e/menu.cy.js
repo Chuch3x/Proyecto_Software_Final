06f5806233ce059a1fdd96072f3a323e5761f8fe
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-05-31 18:23:46 -0400

    Se hizo identación en el código y se arreglaron algunos errores en el cypress

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -1,28 +1,52 @@
 describe("Mostrar Menu", () => {
-    it("Se debería poder ver el menu", () => {
-      cy.visit("index.html");
-      cy.get("#items_menu").should("contain", "Salchipapa");
-    });
-    it('Se debería reservar un producto', () => {
-      cy.visit('index.html')
-      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
-      cy.get('#mostrar_reservas').click()
-      cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 1");
-    });
-    it('Se debería reservar un producto con cantidad 3', () => {
-      cy.visit('index.html')
-      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::input').clear().type(3)
-      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
-      cy.get('#mostrar_reservas').click()
-      cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 3");
-    });
-    it('Se debería eliminar una reserva', () => {
-      cy.visit('index.html')
-      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::input').clear().type(3)
-      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
-      cy.get('#mostrar_reservas').click()
-      cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 3");
-      cy.xpath('id("lista_reservas")//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
-      cy.get("#lista_reservas").should("not.have.text", "Salchipapa - Precio: $10 - Cantidad: 3");
-    });
+  it("Se debería poder ver el menu", () => {
+    cy.visit("index.html");
+    cy.get("#items_menu").should("contain", "Salchipapa");
   });
+  it("Se debería reservar un producto", () => {
+    cy.visit("index.html");
+    cy.xpath(
+      '//li[contains(text(),"Salchipapa")]/following-sibling::button'
+    ).click();
+    cy.get("#mostrar_reservas").click();
+    cy.get("#lista_reservas").should(
+      "contain",
+      "Salchipapa - Precio: $10 - Cantidad: 1"
+    );
+  });
+  it("Se debería reservar un producto con cantidad 3", () => {
+    cy.visit("index.html");
+    cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::input')
+      .clear()
+      .type(3);
+    cy.xpath(
+      '//li[contains(text(),"Salchipapa")]/following-sibling::button'
+    ).click();
+    cy.get("#mostrar_reservas").click();
+    cy.get("#lista_reservas").should(
+      "contain",
+      "Salchipapa - Precio: $10 - Cantidad: 3"
+    );
+  });
+  it("Se debería eliminar una reserva", () => {
+    cy.visit("index.html");
+    cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::input')
+      .clear()
+      .type(3);
+    cy.xpath(
+      '//li[contains(text(),"Salchipapa")]/following-sibling::button'
+    ).click();
+    cy.get("#mostrar_reservas").click();
+    cy.get("#lista_reservas").should(
+      "contain",
+      "Salchipapa - Precio: $10 - Cantidad: 3"
+    );
+    cy.xpath(
+      'id("lista_reservas")//li[contains(text(),"Salchipapa")]/following-sibling::button'
+    ).click();
+    cy.get("#lista_reservas").should(
+      "not.have.text",
+      "Salchipapa - Precio: $10 - Cantidad: 3"
+    );
+  });
+});
--- cypress/e2e/menuAdmin.cy.js
+++ cypress/e2e/menuAdmin.cy.js
@@ -1,33 +1,43 @@
-describe('Menu vista Admin ', () => {
-    it("Muestra un item en el menu", () => {
-        cy.visit("administrador");
-        cy.get("#items_menu").should("contain", "Salchipapa");
-    });
-    it("Debería existir un menu", () => {
-        cy.visit("administrador");
-        cy.get("#items_menu").should("exist");
-    });
-    it('Debería crear producto', () => {
-        cy.visit('administrador.html')
-        cy.get('#nombre_producto').type('Choripan')
-        cy.get('#precio_producto').type('15')    
-        cy.get('#stock_producto').type('10')   
-        cy.get('#descripcion_producto').type('Chorizo en un pan')   
-        cy.get('#categoria_producto').type('snacks')   
-        cy.get('input[value="Crear producto"]').click()
-        cy.get("#items_menu").should("contain", "Choripan: Chorizo en un pan - Precio: $15 - Stock: 10");
-    });
-    it('Debería eliminar producto', () => {
-        cy.visit('administrador.html')
-        cy.get('#nombre_producto').type('Hamburguesa')
-        cy.get('#precio_producto').type('10')    
-        cy.get('#stock_producto').type('10')   
-        cy.get('#descripcion_producto').type('Carne en un pan')   
-        cy.get('#categoria_producto').type('snacks')   
-        cy.get('input[value="Crear producto"]').click()
-        cy.get("#items_menu").should("contain", "Hamburguesa: Carne en un pan - Precio: $10 - Stock: 10");
-        cy.xpath('id("items_menu")//li[contains(text(),"Hamburguesa")]/following-sibling::button/following-sibling::button').click()
-        cy.get("#items_menu").should("not.have.text", "Hamburguesa: Carne en un pan - Precio: $10 - Stock: 10");
-
-    });
+describe("Menu vista Admin ", () => {
+  it("Muestra un item en el menu", () => {
+    cy.visit("administrador");
+    cy.get("#items_menu").should("contain", "Salchipapa");
+  });
+  it("Debería existir un menu", () => {
+    cy.visit("administrador");
+    cy.get("#items_menu").should("exist");
+  });
+  it("Debería crear producto", () => {
+    cy.visit("administrador.html");
+    cy.get("#nombre_producto").type("Choripan");
+    cy.get("#precio_producto").type("15");
+    cy.get("#stock_producto").type("10");
+    cy.get("#descripcion_producto").type("Chorizo en un pan");
+    cy.get("#categoria_producto").type("snacks");
+    cy.get('input[value="Crear producto"]').click();
+    cy.get("#items_menu").should(
+      "contain",
+      "Choripan: Chorizo en un pan - Precio: $15 - Stock: 10"
+    );
+  });
+  it("Debería eliminar producto", () => {
+    cy.visit("administrador.html");
+    cy.get("#nombre_producto").type("Hamburguesa");
+    cy.get("#precio_producto").type("10");
+    cy.get("#stock_producto").type("10");
+    cy.get("#descripcion_producto").type("Carne en un pan");
+    cy.get("#categoria_producto").type("snacks");
+    cy.get('input[value="Crear producto"]').click();
+    cy.get("#items_menu").should(
+      "contain",
+      "Hamburguesa: Carne en un pan - Precio: $10 - Stock: 10"
+    );
+    cy.xpath(
+      'id("items_menu")//li[contains(text(),"Hamburguesa")]/following-sibling::button/following-sibling::button'
+    ).click();
+    cy.get("#items_menu").should(
+      "not.have.text",
+      "Hamburguesa: Carne en un pan - Precio: $10 - Stock: 10"
+    );
+  });
 });
