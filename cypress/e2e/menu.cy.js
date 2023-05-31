5c2a7f0b624107518dc5e96357c99bdc5ac127d1
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-05-31 17:46:06 -0400

    Cypress: Se debería poder eliminar una reserva

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -16,5 +16,13 @@ describe("Mostrar Menu", () => {
       cy.get('#mostrar_reservas').click()
       cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 3");
     });
+    it('Se debería eliminar una reserva', () => {
+      cy.visit('index.html')
+      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::input').clear().type(3)
+      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
+      cy.get('#mostrar_reservas').click()
+      cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 3");
+      cy.xpath('id("lista_reservas")//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
+      cy.get("#lista_reservas").should("contain", "");
+    });
   });
-  //id('lista_reservas')//li[contains(text(),"Salchipapa")]/following-sibling::button
