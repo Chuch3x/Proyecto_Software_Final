f697363120f16303cb3a36daa1d6ec3c97737b02
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-05-31 17:40:10 -0400

    Cypress: Se debería poder reservar un producto

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -3,5 +3,11 @@ describe("Mostrar Menu", () => {
       cy.visit("index.html");
       cy.get("#items_menu").should("contain", "Salchipapa");
     });
+    it('Se debería reservar un producto', () => {
+      cy.visit('index.html')
+      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
+      cy.get('#mostrar_reservas').click()
+      cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 1");
+    });
   });
-  
+  //id('lista_reservas')//li[contains(text(),"Salchipapa")]/following-sibling::button
