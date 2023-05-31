662d75f189b12afdb9559319c2b732067041e0ee
Mateo <mateo>
2023-05-30 21:31:45 -0400

    Refactoring: Movemos la funcion de mostrarProductos al operacionesPresenter para quitar logica de negocio

--- src/presenter.js
+++ src/presenter.js
@@ -1,72 +1,13 @@
 import Cliente from "./cliente";
 import Item from "./item";
+import { mostrarProductos } from "./operacionesPresenter";
 import PRODUCTOS from "./productos";
 
 const form = document.querySelector("#menu_form");
 const div = document.querySelector("#lista_reservas");
 const productos_lista = document.querySelector("#items_menu");
 
-const cliente = new Cliente("contrasena1", "dayan");
-
-function crearBoton(text, clickHandler) {
-  const button = document.createElement("button");
-  button.textContent = text;
-  button.addEventListener("click", clickHandler);
-  return button;
-}
-
-function crearContenedor(className, elements) {
-  const container = document.createElement("div");
-  container.setAttribute("class", className);
-  elements.forEach((element) => container.appendChild(element));
-  return container;
-}
-
-function mostrarProducto(producto) {
-  const li = document.createElement("li");
-  li.innerHTML = `${producto.nombre}: ${producto.descripcion} - Precio: $${producto.precio} - Stock: ${producto.stock}`;
-
-  const inputCantidad = document.createElement("input");
-  inputCantidad.type = "number";
-  inputCantidad.min = "0";
-  inputCantidad.value = "1";
-
-  const agregarButton = crearBoton("Reservar", () => {
-    const cantidad = parseInt(inputCantidad.value);
-    if (cantidad > 0) {
-      cliente.agregarReserva(producto, cantidad);
-      localStorage.setItem("reservas", JSON.stringify(cliente.reservas));
-      console.log(PRODUCTOS);
-      mostrarProductos();
-    }
-  });
-
-  return crearContenedor("item_menu", [li, inputCantidad, agregarButton]);
-}
-
-function mostrarProductos() {
-  productos_lista.innerHTML = "";
-
-  const snacksContainer = document.createElement("div");
-  snacksContainer.innerHTML = "<b>SNACKS</b>";
-
-  const segundosContainer = document.createElement("div");
-  segundosContainer.innerHTML = "<b>SEGUNDOS</b>";
-
-  PRODUCTOS.forEach((producto) => {
-    const container = mostrarProducto(producto);
-    if (producto.categoria === "snacks") {
-      snacksContainer.appendChild(container);
-    } else if (producto.categoria === "segundo") {
-      segundosContainer.appendChild(container);
-    }
-  });
-
-  productos_lista.appendChild(snacksContainer);
-  productos_lista.appendChild(segundosContainer);
-}
-
-mostrarProductos();
+mostrarProductos(productos_lista);
 
 function actualizarItem(lista, producto) {
   const foundItem = lista.find((item) => item.nombre === producto.nombre);
