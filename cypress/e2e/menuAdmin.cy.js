60a060e1b9098018618f4e43eecfdda9945a21cb
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-05-31 17:57:36 -0400

    Cypress: Se debería poder eliminar un producto

--- cypress/e2e/menuAdmin.cy.js
+++ cypress/e2e/menuAdmin.cy.js
@@ -17,4 +17,17 @@ describe('Menu vista Admin ', () => {
         cy.get('input[value="Crear producto"]').click()
         cy.get("#items_menu").should("contain", "Choripan: Chorizo en un pan - Precio: $15 - Stock: 10");
     });
+    it('Debería eliminar producto', () => {
+        cy.visit('administrador.html')
+        cy.get('#nombre_producto').type('Hamburguesa')
+        cy.get('#precio_producto').type('10')    
+        cy.get('#stock_producto').type('10')   
+        cy.get('#descripcion_producto').type('Carne en un pan')   
+        cy.get('#categoria_producto').type('snacks')   
+        cy.get('input[value="Crear producto"]').click()
+        cy.get("#items_menu").should("contain", "Hamburguesa: Carne en un pan - Precio: $10 - Stock: 10");
+        cy.xpath('id("items_menu")//li[contains(text(),"Hamburguesa")]/following-sibling::button/following-sibling::button').click()
+        cy.get("#items_menu").not().should("contain", "");
+
+    });
 });
