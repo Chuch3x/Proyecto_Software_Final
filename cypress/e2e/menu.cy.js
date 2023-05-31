2227d28cbcea85add108444a448f93c6b6f94595
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-05-31 17:41:14 -0400

    Cypress: Se debería poder reservar un producto con cantidad 3

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -9,5 +9,12 @@ describe("Mostrar Menu", () => {
       cy.get('#mostrar_reservas').click()
       cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 1");
     });
+    it('Se debería reservar un producto con cantidad 3', () => {
+      cy.visit('index.html')
+      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::input').clear().type(3)
+      cy.xpath('//li[contains(text(),"Salchipapa")]/following-sibling::button').click()
+      cy.get('#mostrar_reservas').click()
+      cy.get("#lista_reservas").should("contain", "Salchipapa - Precio: $10 - Cantidad: 3");
+    });
   });
   //id('lista_reservas')//li[contains(text(),"Salchipapa")]/following-sibling::button
